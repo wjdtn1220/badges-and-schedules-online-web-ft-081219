5e3e5550a3e3242396a8857b2114d2554c6d8bed
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 01:13:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,11 +23,11 @@ def assign_rooms(names)
 end
 
 def printer(names)
-  batch_badge_creator(names).each do |id|
-    puts id
+  batch_badge_creator(names).each do |assignment|
+    puts assignment
   end 
   
-  assign_rooms(names).each do |id|
-    puts id 
+  assign_rooms(names).each do |assignment|
+    puts assignment
   end
 end 
