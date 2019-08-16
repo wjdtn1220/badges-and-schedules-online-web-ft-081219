463dc2d0b326a27cd99eff6efba010182fb1e630
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 01:12:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(names)
   new_array = []
   names.each do |name|
-    puts new_array << "Hello, my name is #{name}."
+    new_array << "Hello, my name is #{name}."
   end
   new_array
 end
@@ -16,7 +16,7 @@ def assign_rooms(names)
   new_array = []
   room_num = 1
   names.each do |name|
-    puts new_array << "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    new_array << "Hello, #{name}! You'll be assigned to room #{room_num}!"
   room_num +=1
   end
   new_array
