a904234723cfb14c4e939fac25b1b0b6676dc1a8
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 01:07:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,33 +1,14 @@
 # Write your code here.
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(people)
-  new_arry = []
-  people.each do |name|
-    new_arry << ("Hello, my name is #{name}.")
+def batch_badge_creator(names)
+  new_array = []
+  names.each do |name|
+    puts new_array << "Hello, my name is #{name}."
   end
-  return new_arry
+  new_array
 end
 
-def assign_rooms(people)
-  new_arry =[]
-  room_num = 1
-  people.each do |name|
-    new_arry << ("Hello, #{name}! You'll be assigned to room #{room_num}!")
-    room_num += 1
-  end
-  return new_arry
-end
-
-def printer(people)
-  batch_badge_creator(people).each do |resul|
-    puts resul
-  end
-  
-  assign_rooms(people).each do |resul|
-    puts resul
-  end
-end
